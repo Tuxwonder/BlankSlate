e6e10c3b925ce931ae0a03b92f978c7d58f35b91
Walter <wsve1501@iastate.edu>
2017-02-24 21:17:26 -0600

    Added Java workspace and Whiteboard class skeleton

---
+++ Workspace/BlankSlate/src/Whiteboard.java
@@ -0,0 +1,28 @@
+import java.awt.Point;
+
+public class Whiteboard {
+	public Point getLocation()
+	{
+		return null;
+	}
+	
+	public boolean calibrate()
+	{
+		return false;
+	}
+	
+	public void penUp()
+	{
+		
+	}
+	
+	public void penDown()
+	{
+		
+	}
+	
+	public void setXY(Point point)
+	{
+		
+	}
+}
